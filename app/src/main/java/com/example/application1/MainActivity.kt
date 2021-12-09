6f7e6d4573d56d110efec6cbe552fdd0149eab53
4321lok <1234lok@mail.ru>
2021-12-09 16:07:41 +0300

    1

---
+++ app/src/main/java/com/example/application1/MainActivity.kt
@@ -0,0 +1,11 @@
+пpackage com.example.application1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
